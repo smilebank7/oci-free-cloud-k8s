5b54bb72a3859775284f66672b3e9d22f0eb9f90
Jhin <smilebank7@naver.com>
2025-06-24 10:20:53 +0900

    Update _terraform.tf

--- terraform/infra/_terraform.tf
+++ terraform/infra/_terraform.tf
@@ -1,23 +1,16 @@
 terraform {
 
-  # S3 백엔드 임시 비활성화 (x-amz-content-sha256 오류 해결 필요)
-  # backend "s3" {
-  #   bucket = "terraform-states"
-  #   key    = "infra/terraform.tfstate"
-  #   endpoints = {
-  #     # ap-chuncheon-1 리전의 네임스페이스를 확인해서 수정해주세요
-  #     # 형식: https://<namespace>.compat.objectstorage.<region>.oraclecloud.com
-  #     s3 = "https://axejwclweluc.compat.objectstorage.ap-chuncheon-1.oraclecloud.com"
-  #   }
-  #   region                      = "ap-chuncheon-1"
-  #   shared_credentials_files    = ["~/.aws/credentials"]
-  #   skip_region_validation      = true
-  #   skip_credentials_validation = true
-  #   skip_requesting_account_id  = true
-  #   skip_metadata_api_check     = true
-  #   skip_s3_checksum            = true
-  #   use_path_style              = true
-  # }
+  backend "s3" {
+    bucket = "terraform-states"
+    key    = "infra/terraform.tfstate"
+    endpoint = "https://axejwclweluc.compat.objectstorage.ap-chuncheon-1.oraclecloud.com"
+    region = "ap-chuncheon-1"
+    shared_credentials_file     = "~/.oci/config"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
 
   required_providers {
     jq = {
